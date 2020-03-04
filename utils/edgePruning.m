edf9943b56448be85550d10c526d3c02df5835d9
dharmatarha <adam.boncz@gmail.com>
2020-03-04 05:49:01 +0100

    - introduced parfor for the loop over subjects - added support for data decimation - added input arg for no. of surrogate data sets - saves results per subject

--- utils/edgePruning.m
+++ utils/edgePruning.m
@@ -2,7 +2,7 @@ function edgePruning(freq, varargin)
 
 %% Select significant edges compared to a surrogate null model
 %
-% USAGE: edgePruning(freq, dirName = pwd, subjects = {'s02', 's03', ...}, method = 'plv')
+% USAGE: edgePruning(freq, dirName = pwd, subjects = {'s02', 's03', ...}, method = 'plv', dRate = 1, surrNo = 10000)
 %
 % Calculates p-values of edges in the connectivity matrix based on
 % a phase scrambling based surrogate null model.
@@ -12,14 +12,14 @@ function edgePruning(freq, varargin)
 %
 % Results are saved into the provided
 % directory (or into current working directory), named
-% 'FREQUENCYBAND_angleConnectivity.mat'.
+% 'FREQUENCYBAND_edgePruningResults.mat'.
 %
 % Assumes that the data is in EEGlab structures and that file naming
 % follows the 'SUBJECTNUMBER_FREQUENCYBAND.mat' (e.g. 's05_alpha.mat')
 % convention
 % Also assumes that all relevant files are in the working directory
 % 
-% Optional input args are inferred from input arg types.
+% Optional input args are inferred from input arg types and values.
 %
 % Mandatory input:
 % freq      - Frequency band (string) to work with. Needs to be the same 
@@ -28,10 +28,10 @@ function edgePruning(freq, varargin)
 %        'theta'}
 % 
 % Optional inputs:
-% dirName   - Directory path (string) pointing to the data files. Also 
-%       used for saving out group means. Default is current working 
-%       directory (pwd).
-% subjects  - List of subjects (cell array) whose data we average. 
+% dirName   - Directory path (string) pointing to 'angle' and 'envelope' 
+%       folders. Also used for creating a 'connectivity' folder for saving 
+%       out results. Default is current working directory (pwd).
+% subjects  - List of subjects (cell array) whose data we process. 
 %       Each cell contains a subject ID also used in the filenames 
 %       (e.g. 's01' for files like 's01_alpha.mat'). If empty, 
 %       we use the default cell arrray of:
@@ -39,7 +39,17 @@ function edgePruning(freq, varargin)
 %           ,'s11','s12','s13','s14','s15','s16','s17','s18','s19','s20',...
 %           's21','s22','s23','s24','s25','s26','s27','s28'}
 % method    - Connectivity measure compatible with phase data, one of 
-%           {'plv', 'pli'}. Defaults to 'plv'.
+%       {'plv', 'pli'}. Defaults to 'plv'.
+% dRate     - Decimation rate. We usually work with bandpass-filtered data
+%       that nevertheless retains the original (1000 or 500 Hz) sampling
+%       rate. For efficiency, we can decimate this data by providing a
+%       dRate integer. E.g. dRate = 10 corresponds to one 10th of the data
+%       remaining after decimation. Integer between 1-20, defaults to 1 
+%       (no decimation). NOTE THAT THERE IS NO ADDITIONAL FILTERING STEP 
+%       INCLUDED, BEWARE OF ALIASING EFFECTS
+% surrNo    - Number of surrogate data sets generated for statistical
+%       testing of edge values. Integer, one of [100:100:20000], defaults 
+%       to 10000. 
 % 
 %  
 
@@ -48,10 +58,10 @@ function edgePruning(freq, varargin)
 
 % check for mandatory argument
 if nargin < 1
-    error('Input arg frequency band is required!');
+    error('Input arg "freq" (frequency band) is required!');
 end
 if ~ismember(freq, {'delta', 'theta', 'alpha', 'beta', 'gamma'})
-    error('Input arg freq has an unexpected value!');
+    error('Input arg "freq" has an unexpected value!');
 end
 
 % check optional arguments
@@ -63,10 +73,14 @@ if ~isempty(varargin)
         elseif ischar(varargin{v}) && ~exist('dirName', 'var') && exist(varargin{v}, 'dir')
             dirName = varargin{v};
         elseif ischar(varargin{v}) && ~exist('method', 'var') && ismember(varargin{v}, {'pli', 'plv'})
-            method = varargin{v};           
+            method = varargin{v};     
+        elseif isnumeric(varargin{v}) && ~exist('dRate', 'var') && ismember(varargin{v}, 1:20)
+            dRate = varargin{v};
+        elseif isnumeric(varargin{v}) && ~exist('surrNo', 'var') && ismember(varargin{v}, 100:100:20000)
+            surrNo = varargin{v};
         else
             error(['There are either too many input args or they are not ',...
-                'mapping nicely to "dirName", "subjects" and "method"!']);
+                'mapping nicely to "dirName", "subjects", "method" and "dRate"!']);
         end
     end
 end
@@ -83,14 +97,28 @@ end
 if ~exist('method', 'var')
     method = 'plv';
 end
+if ~exist('dRate', 'var')
+    dRate = 1;
+end
+if ~exist('surrNo', 'var')
+    surrNo = 10000;
+end
 
 % user message
-disp([char(10), 'Starting angleConnectivityWrapper function with following arguments: ',...
+disp([char(10), 'Starting edgePruning function with following arguments: ',...
     char(10), 'Frequency band: ', freq,...
     char(10), 'Working directory: ', dirName,...
     char(10), 'Connectivity measure: ', method,...
+    char(10), 'Decimation rate: ', num2str(dRate),...
+    char(10), 'No. of surrogate data sets: ', num2str(surrNo), ... 
     char(10), 'Subjects: ']);
 disp(subjects);
+
+
+%% Init parallel pool
+
+% use default settings
+myPool = parpool();
 
 
 %% Basics
@@ -98,31 +126,58 @@ disp(subjects);
 % number of subjects
 subNo = length(subjects);
 
-% load first data, check dimensions
-checkFile = [dirName, '/', subjects{1}, '_', freq, '.mat'];
-load(checkFile, 'EEG');
-[roiNo, sampleNo, epochNo, stimNo] = size(EEG.data);
-% sanity check
-if any(any(any(any(EEG.data>pi, 1), 2), 3), 4) || any(any(any(any(EEG.data<-pi, 1), 2), 3), 4)
+% angle and envelope folders
+angleDir = [dirName, '/angle/', freq, '/'];
+envDir = [dirName, '/envelope/', freq, '/'];
+
+% add all angle and envelope files we will use to parallel loop
+angleFiles = cell(subNo, 1);
+envFiles = cell(subNo, 1);
+for s = 1:subNo
+    angleFiles{s} = [angleDir, subjects{s}, '_', freq, '.mat'];
+    envFiles{s} = [envDir, subjects{s}, '_', freq, '.mat'];
+end
+% add files to parallel pool
+addAttachedFiles(myPool, angleFiles);
+addAttachedFiles(myPool, envFiles);
+
+% load first data files, both angle and envelope, check dimensions
+disp([char(10), 'Sanity checks on the first angle and envelope data sets...']);
+phaseData = load(angleFiles{1}, 'EEG');
+phaseData = phaseData.EEG.data;
+[roiNo, sampleNo, epochNo, stimNo] = size(phaseData);
+% sanity check - is it phase data?
+if any(any(any(any(phaseData>pi, 1), 2), 3), 4) || any(any(any(any(phaseData<-pi, 1), 2), 3), 4)
     error('There is data here outside the [-pi +pi] range, is it really phase data?');
+end
+% error if angle data has different size than envelope data
+envData = load(envFiles{1}, 'EEG');
+envData = envData.EEG.data;
+if ~isequal([roiNo, sampleNo, epochNo, stimNo], size(envData))
+    error('Angle and envelope data matrices have different dimensions, investigate!');
+end
+
+% decimation 
+if dRate ~= 1
+    phaseData = phaseData(:, 1:dRate:end, :, :);
+    sampleNoOrig = sampleNo;
+    sampleNo = size(phaseData, 2);
 end
 
 % user message
-disp([char(10), 'First data file had ', num2str(roiNo), ' channels/ROIs, ',... 
-    num2str(stimNo), ' different stimuli (stories), ',...
-    num2str(epochNo), ' epochs and ', num2str(sampleNo), ... 
-    ' samples for each epoch. Assuming same dimensions for each data file.']);
-
-% Preallocate result matrix
-connectivityRes = nan(subNo, stimNo, epochNo, roiNo, roiNo);
-measuredConnectivityRes = nan(subNo, stimNo, epochNo, roiNo, roiNo);
-prunedMeasuredConnectivityRes = nan(subNo, stimNo, epochNo, roiNo, roiNo);
-meanSurrogateConnectivityRes = nan(subNo, stimNo, epochNo, roiNo, roiNo);
-surrogatePValues = nan(subNo, stimNo, epochNo, roiNo, roiNo);
-
-
-% save file
-saveF = [dirName, '/' , freq, '_edgePruningInfo.mat'];
+if dRate == 1
+    disp([char(10), 'First data file had ', num2str(roiNo), ' channels/ROIs, ',... 
+        num2str(stimNo), ' different stimuli (stories), ',...
+        num2str(epochNo), ' epochs and ', num2str(sampleNo), ... 
+        ' samples for each epoch. Assuming same dimensions for each data file.']);
+else
+    disp([char(10), 'First data file had ', num2str(roiNo), ' channels/ROIs, ',... 
+        num2str(stimNo), ' different stimuli (stories), ',...
+        num2str(epochNo), ' epochs and ', num2str(sampleNoOrig), ... 
+        ' samples for each epoch. Assuming same dimensions for each data file.']);
+    disp([char(10), 'After decimation (dRate = ', num2str(dRate), '), there ',...
+        'are ', num2str(sampleNo), ' samples for each epoch.']);
+end
 
 % clock for timing the full function run
 funcClock = tic;
@@ -130,7 +185,7 @@ funcClock = tic;
 
 %% Loop through subjects
 
-for subIdx = 1:subNo
+parfor subIdx = 1:subNo
     
     % user message
     disp([char(10), 'Working on data from subject ', subjects{subIdx}]);
@@ -138,48 +193,93 @@ for subIdx = 1:subNo
     % for timekeeping start a subject-level clock
     subClock = tic;
     
-    % load angle data - for the first subject we have already loaded it in
-    % previous block
-    if subIdx ~= 1%
-        subFile = [dirName, '/', subjects{subIdx}, '_', freq,'.mat'];
-        load(subFile, 'EEG');
-        % check data size
-        if ~isequal(size(EEG.data), [roiNo, sampleNo, epochNo, stimNo])
-            error(['Data for subject ', subjects{subIndex}, ' has unexpected size, investigate!']);
-        end
-        % sanity check - is it really phase data, i.e. between -pi +pi
-        if any(any(any(any(EEG.data>pi, 1), 2), 3), 4) || any(any(any(any(EEG.data<-pi, 1), 2), 3), 4)
-            error('There is data here outside the [-pi +pi] range, is it really phase data?');
-        end
+    % load angle and envelope data - for the first subject we have already 
+    % loaded it in previous block, but due to how parfor works we need to
+    % do so here again:
+    
+    % angle data
+    phaseData = load(angleFiles{subIdx}, 'EEG');
+    phaseData = phaseData.EEG.data;
+    if dRate ~= 1
+        phaseData = phaseData(:, 1:dRate:end, :, :);
     end
+    % check data size
+    if ~isequal(size(phaseData), [roiNo, sampleNo, epochNo, stimNo])
+        error(['Angle data for subject ', subjects{subIndex}, ' has unexpected size, investigate!']);
+    end
+    % sanity check - is it really phase data, i.e. between -pi +pi
+    if any(any(any(any(phaseData>pi, 1), 2), 3), 4) || any(any(any(any(phaseData<-pi, 1), 2), 3), 4)
+        error('There is data here outside the [-pi +pi] range, is it really phase data?');
+    end
+    % envelope data
+    envData = load(envFiles{subIdx}, 'EEG');
+    envData = envData.EEG.data;    
+    if dRate ~= 1
+        envData = envData(:, 1:dRate:end, :, :);
+    end
+    % check data size
+    if ~isequal(size(envData), [roiNo, sampleNo, epochNo, stimNo])
+        error(['Envelope data for subject ', subjects{subIdx}, ' has unexpected size, investigate!']);
+    end        
+    
+    % preallocate result matrices
+    realConn = nan(roiNo, roiNo, epochNo, stimNo);  % for storing the real connectivity matrix for each epoch
+    meanSurrConn = nan(roiNo, roiNo, epochNo, stimNo);  % for storing the mean connectivity matrix from surrogate datasets
+    pValues = nan(roiNo, roiNo, epochNo, stimNo);  % for storing the p-values from the comparisons between real and surrogate connectivity values
+    prunedConn = nan(roiNo, roiNo, epochNo, stimNo);  % for storing the pruned connectivity matrices
     
     % loop through stimulus type / stories
     for stimIdx = 1:stimNo       
         % loop through epochs
         for epochIdx = 1:epochNo
             
+            % get real-valued time series data from polar analytic
+            [realData, ~] = pol2cart(squeeze(phaseData(:, :, epochIdx, stimIdx)), squeeze(envData(:, :, epochIdx, stimIdx))); 
+            
             % measure depends on the arg method
             switch method    
                 case 'plv'
-                    eegData = squeeze(EEG.data(:, :, epochIdx, stimIdx));
-                    phaseData = timeSeriesToPhase(eegData);
-                    measuredConnectivityRes(subIdx, stimIdx, epochIdx, :, :) = plv(phaseData, 0);  % suppress messages from plv function
-                    tic
-                    surrogateConnectivityRes = generateSurrogateConnectivityData(eegData);
-                    toc
-                    meanSurrogateConnectivityRes(subIdx, stimIdx, epochIdx, :, :) = mean(surrogateConnectivityRes, 1);
-                    surrogatePValues(subIdx, stimIdx, epochIdx, :, :) = surrogateConnectivityStatistics(squeeze(measuredConnectivityRes(subIdx, stimIdx, epochIdx, :, :)), surrogateConnectivityRes);
-                    prunedMeasuredConnectivityRes(subIdx, stimIdx, epochIdx, :, :)  = pruningFunction(squeeze(surrogatePValues(subIdx, stimIdx, epochIdx, :, :)), squeeze(measuredConnectivityRes(subIdx, stimIdx, epochIdx, :, :)));
+                    
+                    % calculate real connectivity results
+                    realConn(:, :, epochIdx, stimIdx) = plv(squeeze(phaseData(:, :, epochIdx, stimIdx)), 0);
+                    
+                    % generate surrogate datasets and calculate
+                    % connectivity matrices for them
+                    surrConnRes = generateSurrogateConnectivityData(realData, surrNo);
+                    % store the mean
+                    meanSurrConn(:, :, epochIdx, stimIdx) = squeeze(mean(surrConnRes, 1));
+                    
+                    % compare the real and surrogate connectivity matrices,
+                    % estimate p-values for each value
+                    pValues(:, :, epochIdx, stimIdx) = surrogateConnectivityStatistics(squeeze(realConn(:, :, epochIdx, stimIdx)), surrConnRes);
+                    
+                    % edge pruning based on pValues
+                    prunedConn(:, :, epochIdx, stimIdx) = pruningFunction(squeeze(pValues(:, :, epochIdx, stimIdx)), squeeze(realConn(:, :, epochIdx, stimIdx)));
 
                 case 'pli'
-                    connectivityRes(subIdx, stimIdx, epochIdx, :, :) = pli(squeeze(EEG.data(:, :, epochIdx, stimIdx)), 0);  % suppress messages from pli function
+                    % NOT DONE YET
+                    disp('PLI is not really supported here yet....');
+%                     connectivityRes(subIdx, stimIdx, epochIdx, :, :) = pli(squeeze(EEG.data(:, :, epochIdx, stimIdx)), 0);  % suppress messages from pli function
+                    
             end
             
         end  % epochIdx for loop
     end  % stimIdx for loop
     
-    % interim save
-    % save(saveF, 'measuredConnectivityRes', 'meanSurrogateConnectivityRes', 'surrogatePValues', 'freq', 'dirName', 'method', 'subjects');
+    % get a subject-specific file name for saving results
+    saveF = [dirName, '/' , subjects{subIdx}, '_', freq, '_edgePruningInfo.mat'];
+    
+    % save results using matfile - this is mostly allowed in a parfor loop,
+    % unlike the save command
+    saveM = matfile(saveF);
+    saveM.realConn = realConn;
+    saveM.meanSurrConn = meanSurrConn;
+    saveM.pValues = pValues;
+    saveM.prunedConn = prunedConn;
+    saveM.method = method;
+    saveM.surrNo = surrNo;
+    saveM.dRate = dRate;
+    saveM.subject = subjects{subIdx};
     
     % report elapsed time
     disp([char(10), 'Finished with subject ', subjects{subIdx},...
@@ -189,8 +289,6 @@ end  % subIdx for loop
                     
 
 %% Saving, cleaning up
-
-save(saveF, 'measuredConnectivityRes', 'prunedMeasuredConnectivityRes', 'meanSurrogateConnectivityRes', 'surrogatePValues', 'freq', 'dirName', 'method', 'subjects');
 
 disp([char(10), 'Done! Took ', num2str(toc(funcClock), 3),... 
     ' secs for the whole set']);
