8c2d1a91c97c09dc639b7f4af1818163e5028892
dharmatarha <adam.boncz@gmail.com>
2020-03-28 22:22:13 +0100

    roi name loading, small adjustments

--- python/mat2graph.py
+++ python/mat2graph.py
@@ -1,50 +1,94 @@
 from scipy.io import loadmat
-import igraph
+import igraph as ig
+import leidenalg as la
 import numpy as np
 
-def mat2graph(matFile, arrayName='prunedConn', nanValue=0, expShape=(62, 62, 40, 4)):
-    '''
+
+def mat2graph(matfile,
+              arrayname='prunedConn',
+              nanvalue=0,
+              expshape=(62, 62, 40, 4),
+              vertexnames=None):
+    """
     Loads a mat file containing adjacency matrices
     and converts matrices to weighted non-directed graphs
+    (igraph Graph objects)
 
     Inputs:
-    matFile     - Valid path to a .mat file, up tp version -v7
-    arrayName   - Name of the array containing the adjacency
-        matrices of interest, defaults to 'prunedConn'
-    '''
+    matfile     -- Valid path to a .mat file, up tp version -v7
+    arrayname   -- Name of the array containing the adjacency
+            matrices of interest, defaults to 'prunedConn'
+    nanvalue    -- NaN values are changed to "nanValue", defaults to 0
+    expshape    -- Tuple containing the expected shape of the array
+            containing adjacency matrices, defaults to (62, 62, 40, 4)
+    vertexnames -- List of strings, names of vertices. Defaults to None
+
+    Outputs:
+    graphs      -- List of lists, where each element is a list for a condition.
+        In each condition list, there is a Graph object for each epoch.
+    """
 
     # load data, select adjacency matrices
-    matDict = loadmat(matFile)  # returns a dict where keys are variable names
-    adjArray = matDict[arrayName]
+    matdict = loadmat(matfile)  # returns a dict where keys are variable names
+    adjarray = matdict[arrayname]
+
     # check shape
-    if adjArray.shape != expShape:
-        print('Adjacency array has unexpected shape:', str(adjArray.shape))
+    if adjarray.shape != expshape:
+        print('Adjacency array has unexpected shape:', str(adjarray.shape))
+
     # convert nans to zero
-    adjArray[np.isnan(adjArray)] = nanValue
+    adjarray[np.isnan(adjarray)] = nanvalue
 
     # convert each adjacency matrix into a graph
     graphs = []  # init a list that will hold lists of epoch-level graphs
-    for stim in range(expShape[-1]):  # loop through conditions
+    for stim in range(expshape[-1]):  # loop through conditions
+
         epochs = []  # init a list for epoch-level graphs
-        for epoch in range (expShape[-2]):  # loop through epochs
+        for epoch in range(expshape[-2]):  # loop through epochs
+
             # select adj matrix for given epoch in given condition
-            adjTmp = adjArray[:, :, epoch, stim]
+            adjtmp = adjarray[:, :, epoch, stim]
+
             # create graph object from a boolean version
-            g = igraph.Graph.Adjacency((adjTmp > 0).tolist())
-            g.es['weight'] = adjTmp[adjTmp.nonzero()]  # add weights from adj matrix
+            g = ig.Graph.Adjacency((adjtmp > 0).tolist())
+            g.es['weight'] = adjtmp[adjtmp.nonzero()]  # add weights from adj matrix
             g.to_undirected()  # cast into undirected graph
+
+            # set vertex names if the input arg was supplied
+            if vertexnames:
+                g.vs['label'] = vertexnames
+
             epochs.append(g)
         graphs.append(epochs)
 
     return graphs
 
 
+def mat2roinames(roinamesfile, roinamesvar='rois'):
+    """
+    Loads a mat file containing ROI (graph vertex) names in a cell array.
 
+    Inputs:
+    roinamesfile  -- Path to .mat file containing ROI names
+            in a cell array
+    roinamesvar   -- String, name of variable (dict key) containing
+            the ROI names
 
+    Outputs:
+    roilist       -- List of strings, each string corresponds to a ROI
+            (graph vertex)
+    """
 
+    # load data, select roi names variable
+    matdict = loadmat(roinamesfile)
+    rois = matdict[roinamesvar]
 
+    # transform into a list of strings
+    roilist = []
+    tmp = rois[0].tolist()
+    [roilist.append(roi.tolist()[0]) for roi in tmp]
 
-
+    return roilist
 
 
 
