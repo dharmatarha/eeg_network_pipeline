775641909dbe5ab3d0e1225ca18c1bad0d750e63
dharmatarha <adam.boncz@gmail.com>
2020-04-07 13:49:53 +0200

    finished version of connectivity similarity comparisons for full connectivity matrices

--- stats/cmpFullConn.m
+++ stats/cmpFullConn.m
@@ -1,23 +1,63 @@
-function cmpFullConn(connData, metric, perm)
+function [permRes, withinCondPermRes, connSim] = cmpFullConn(connData, varargin)
 %% Compare full connectivity matrices across conditions
 %
-% USAGE: cmpFullConn(connData, metric='corr', surrNo=10000)
+% USAGE: [permRes, withinCondPermRes, connSim, dataLin] = cmpFullConn(connData, metric='corr', permNo=10000, permStat='mean')
 %
-% Compares connectivity patterns across different conditions using
-% permutation tests. 
+% Compares connectivity patterns across different groups of epoch-pairings 
+% using permutation tests. First, for each condition, it tests the
+% within-condition epoch-pairings against the across-condition epoch
+% pairings. Second, for each condition-pairing, it tests if epoch-pairings
+% in one condition have different values than in the other condition 
+% (as in the case of pairwise comparisons). 
 %
 % Input data is expected to contain undirected connectivity values. From
 % each connectivity matrix we only take the upper triangle into account.
 %
-% Inputs:
+% For the tests we rely on permTest.m, input args "permNo" and "permStat"
+% (if specified) are passed to permTest.m.
+%
+% Mandatory input:
 % connData      - Numerical tensor, sets of connectivity matrices across
 %               epochs and conditions (stimuli). First two dimensions have
 %               equal size and determine a connectivity matrix, third
 %               dimension is epochs, fourth is conditions (stimuli).
+%
+% Optional inputs:
 % metric        - String specifying distance metric for connectivity
 %               matrix comparisons. Matrices are first vectorized, 
 %               then one of these ditances is used: {'corr', 'eucl'}.  
+% permNo        - Numeric value, the number of permutations to perform for
+%               random permutation tests. One of 1:1000000 - the values
+%               supported by permTest.m
+% permStat      - String specifying the statistic we perform the random
+%               permutaiton tests on. One of {'mean', 'median', 'std'} -
+%               the ones supported by permTest.m
 %
+% Outputs:
+% permRes       - Struct, random permutation test results. Each element of
+%               permRes summerizes the results of the within-condition 
+%               epoch pairings versus across-condition epoch-pairings test 
+%               for a condition, i.e. its size is [1 "numberOfConditions"].
+%               Has fields for the mean, median and SD values of both
+%               epoch-pairing groups, and for the outcomes of permTest,
+%               i.e. estimated p-value, real test stat difference and
+%               permuted difference values.
+% withinCondPermRes       - Struct, random permutation test results. 
+%               Each element of withinCondPermRes summerizes the results of
+%               a comparison of epoch-pairings across two conditions, i.e. 
+%               its size is [1 nchoosek("numberOfConditions", 2)].
+%               Has fields for the mean, median and SD values of both
+%               epoch-pairing groups, and for the outcomes of permTest,
+%               i.e. estimated p-value, real test stat difference and
+%               permuted difference values.
+% connSim       - Numeric matrix containing connectivity similarity values
+%               for all epoch-pairings. Symmetric, as the upper triangle
+%               values are calculated and mirrored to the lower half. Its
+%               size is ["numbreOfEpochs"*"numberOfConditions",
+%               "numbreOfEpochs"*"numberOfConditions"].
+%
+%
+
 
 %% Input checks
 
@@ -80,16 +120,19 @@ end
 % becomes epoch 22
 dataLin = reshape(dataLin, [(roiNo*roiNo-roiNo)/2, epochNo*condNo]);
 
+% user message
+disp([char(10), 'Linearized data']);
+
 
 %% Get similarity across all epoch-pairings
 
 % preallocate
-connSim = nan(epochNo*epochNo);  % connectivity pattern similarity across epochs
+connSim = nan(epochNo*condNo);  % connectivity pattern similarity across epochs
 pairCounter = 0;  % counter for pairings calculated
-pastPairings = zeros((epochNo*epochNo-epochNo)/2, 2);  % var storing epoch pairings calculated
+pastPairings = zeros((epochNo^2*condNo^2-epochNo*condNo)/2, 2);  % var storing epoch pairings calculated
 % loops through all epochs
-for epochOne = 1:epochNo
-    for epochTwo = 1:epochNo
+for epochOne = 1:epochNo*condNo
+    for epochTwo = 1:epochNo*condNo
         % only consider epoch-pairing if the two epochs are not the same
         % and have not been encountered before
         if epochOne ~= epochTwo && ~ismember([epochOne, epochTwo], pastPairings, 'rows') && ~ismember([epochTwo, epochOne], pastPairings, 'rows')
@@ -107,12 +150,27 @@ for epochOne = 1:epochNo
     end
 end
 
+% user message
+disp([char(10), 'Calculated similarity across all epoch-pairings']);
+
 
 %% Compare similarity values between within- and across-condition pairings
 
 % first mirror the upper triangular part of connSim matrix to the lower
 % half
 connSim = triu(connSim, 1) + triu(connSim, 1)'; 
+
+% preallocate results struct
+permRes = struct;
+permRes.withinCondMean = nan;
+permRes.withinCondSD = nan;
+permRes.withinCondMedian = nan;
+permRes.acrossCondMean = nan;
+permRes.acrossCondSD = nan;
+permRes.acrossCondMedian = nan;
+permRes.realDiff = nan;
+permRes.permDiff = nan(perm, 1);
+permRes.pEst = nan;
 
 for cond = 1:condNo
     
@@ -141,15 +199,102 @@ for cond = 1:condNo
         tmpAll = [tmpParts{1}, tmpParts{2}];
     end
     % linearize into a vector
-    acrossConSim = reshape(tmpAll', [1, epochNo*(condNo-1)]);
+    acrossCondSim = reshape(tmpAll', [1, epochNo^2*(condNo-1)]);
+    
+    % store descriptives in result struct
+    permRes(cond).withinCondMean = mean(withinCondSim);
+    permRes(cond).withinCondSD = std(withinCondSim);
+    permRes(cond).withinCondMedian = median(withinCondSim);
+    permRes(cond).acrossCondMean = mean(acrossCondSim);
+    permRes(cond).acrossCondSD = std(acrossCondSim); 
+    permRes(cond).acrossCondMedian = median(acrossCondSim);
     
     % permutation test across the two groups of connectivity similarity
     % values
-    [pEst, realDiff, permDiff] = permTest(withinCondSim, acrossConSim, perm);
-    
-    % collect results into aggregate matrices
+    [permRes(cond).pEst, permRes(cond).realDiff,... 
+        permRes(cond).permDiff] = permTest(withinCondSim, acrossCondSim, permNo, permStat);
     
 end
+
+% user message
+disp([char(10), 'Compared similarity within- versus across-condition epoch-pairings']);
+
+
+%% Compare similarity values between different within-condition pairings
+% e.g. between epoch-pairings in within cond 1 and epoch-pairings within
+% cond 2
+
+% preallocate results struct
+withinCondPermRes = struct;
+withinCondPermRes.condOneMean = nan;
+withinCondPermRes.condOneSD = nan;
+withinCondPermRes.condOneMedian= nan;
+withinCondPermRes.condTwoMean = nan;
+withinCondPermRes.condTwoSD = nan;
+withinCondPermRes.condTwoMedian = nan;
+
+% counter for condition pairings
+pairCounter = 0;
+% var storing condition pairings already calculated
+pastPairings = zeros(nchoosek(4, 2), 2);
+
+% loops through conditions
+for condOne = 1:condNo
+    for condTwo = 1:condNo
+        % only calculate if the two conditions are not the same and have
+        % not been take into account yet
+        if condOne~=condTwo && ~ismember([condOne, condTwo], pastPairings, 'rows') && ~ismember([condTwo, condOne], pastPairings, 'rows')
+            pairCounter = pairCounter+1;
+            pastPairings(pairCounter, :) = [condOne, condTwo];
+            
+            % within-cond epoch pairing similarities for condition one
+            tmp = connSim((condOne-1)*epochNo+1:condOne*epochNo, (condOne-1)*epochNo+1:condOne*epochNo);
+            tmp = triu(tmp, 1);  
+            % linearize to vector
+            tmpT = tmp';
+            idx = tril(true(size(tmpT)), -1);
+            withinCondSimOne = tmpT(idx)';
+
+            % within-cond epoch pairing similarities for condition two
+            tmp = connSim((condTwo-1)*epochNo+1:condTwo*epochNo, (condTwo-1)*epochNo+1:condTwo*epochNo);
+            tmp = triu(tmp, 1);  
+            % linearize to vector
+            tmpT = tmp';
+            idx = tril(true(size(tmpT)), -1);
+            withinCondSimTwo = tmpT(idx)';
+            
+            % store descriptives in results struct
+            withinCondPermRes(pairCounter).condOneMean = mean(withinCondSimOne);
+            withinCondPermRes(pairCounter).condOneSD = std(withinCondSimOne);
+            withinCondPermRes(pairCounter).condOneMedian = median(withinCondSimOne);
+            withinCondPermRes(pairCounter).condTwoMean = mean(withinCondSimTwo);
+            withinCondPermRes(pairCounter).condTwoSD = std(withinCondSimTwo);
+            withinCondPermRes(pairCounter).condTwoMedian = median(withinCondSimTwo);
+            
+            % permutation test across the two groups of connectivity similarity
+            % values
+            [withinCondPermRes(pairCounter).pEst, withinCondPermRes(pairCounter).realDiff,... 
+                withinCondPermRes(pairCounter).permDiff] = permTest(withinCondSimOne, withinCondSimTwo, permNo, permStat);
+            
+        end
+        
+    end
+end
+
+% user message
+disp([char(10), 'Compared similarity across conditions for within-condition epoch-pairings']);
+disp('Done with everything!');
+
+return
+
+
+
+
+
+
+
+
+
 
 
 
