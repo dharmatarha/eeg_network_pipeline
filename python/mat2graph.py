bc6d62313a12731c4d6056816fc42740462c3bac
dharmatarha <adam.boncz@gmail.com>
2020-04-21 17:14:02 +0200

    half-finished work on plotting communities

--- python/mat2graph.py
+++ python/mat2graph.py
@@ -2,7 +2,7 @@ from scipy.io import loadmat
 import igraph as ig
 import leidenalg as la
 import numpy as np
-
+from random import randint
 
 def mat2graph(matfile,
               arrayname='prunedConn',
@@ -36,7 +36,7 @@ def mat2graph(matfile,
     if adjarray.shape != expshape:
         print('Adjacency array has unexpected shape:', str(adjarray.shape))
 
-    # convert nans to zero
+    # convert nans to specified value
     adjarray[np.isnan(adjarray)] = nanvalue
 
     # convert each adjacency matrix into a graph
@@ -51,8 +51,8 @@ def mat2graph(matfile,
 
             # create graph object from a boolean version
             g = ig.Graph.Adjacency((adjtmp > 0).tolist())
-            g.es['weight'] = adjtmp[adjtmp.nonzero()]  # add weights from adj matrix
             g.to_undirected()  # cast into undirected graph
+            g.es['weight'] = adjtmp[adjtmp.nonzero()]  # add weights from adj matrix
 
             # set vertex names if the input arg was supplied
             if vertexnames:
@@ -90,6 +90,59 @@ def mat2roinames(roinamesfile, roinamesvar='rois'):
 
     return roilist
 
+def la_partition(g):
+
+    p = la.find_partition(g, la.ModularityVertexPartition, weights=g.es['weight'], n_iterations=-1)
+
+    return p
+
+def plot_communities(g, p):
+
+    return
+
+def _plot(g, membership=None):
+    if membership is not None:
+        gcopy = g.copy()
+        edges = []
+        edges_colors = []
+        for edge in g.es():
+            if membership[edge.tuple[0]] != membership[edge.tuple[1]]:
+                edges.append(edge)
+                edges_colors.append("gray")
+            else:
+                edges_colors.append("black")
+        gcopy.delete_edges(edges)
+        layout = gcopy.layout("kk")
+        g.es["color"] = edges_colors
+    else:
+        layout = g.layout("kk")
+        g.es["color"] = "gray"
+    visual_style = {}
+    visual_style["vertex_label_dist"] = 0
+    visual_style["vertex_shape"] = "circle"
+    visual_style["edge_color"] = g.es["color"]
+    # visual_style["bbox"] = (4000, 2500)
+    visual_style["vertex_size"] = 30
+    visual_style["layout"] = layout
+    visual_style["bbox"] = (1024, 768)
+    visual_style["margin"] = 40
+    visual_style["edge_label"] = g.es["weight"]
+    for vertex in g.vs():
+        vertex["label"] = vertex.index
+    if membership is not None:
+        colors = []
+        for i in range(0, max(membership) + 1):
+            colors.append('%06X' % randint(0, 0xFFFFFF))
+        for vertex in g.vs():
+            vertex["color"] = str('#') + colors[membership[vertex.index]]
+        visual_style["vertex_color"] = g.vs["color"]
+    ig.plot(g, **visual_style)
+
+ #   if __name__ == "__main__":
+ #       g = igraph.Nexus.get("karate")
+ #       cl = g.community_fastgreedy()
+ #       membership = cl.as_clustering().membership
+ #       _plot(g, membership)
 
 
 
