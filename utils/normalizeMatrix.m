c12f50181e54b50a275026be7a0d40ea5c5b816a
penagydrl <60607094+penagydrl@users.noreply.github.com>
2020-05-23 17:17:25 +0200

    Comments were added

--- utils/normalizeMatrix.m
+++ utils/normalizeMatrix.m
@@ -1,5 +1,25 @@
 function [normalizedMatrix] = normalizeMatrix(inputMatrix)
+%% Matrix normalization
+% 
+% USAGE: normalizedMatrix = normalizeMatrix(inputMatrix)
+%
+% Matrix normalization is carried out such that the sum of all elements in the normalized matrix should be one.
+%
+% Mandatory input:
+% inputMatrix       - The matrix to be normalized
+%
+% Output:
+% normalizedMatrix  - Normalized matrix
+%
 
+%% Input checks
+
+% Check if the input is a matrix
+if ~ ismatrix(inputMatrix)
+    error('Input shoud be a matrix');
+end
+
+%% Calculation
 sumOfAllElements = sum(sum(inputMatrix));
 normalizedMatrix = inputMatrix / sumOfAllElements;
 
