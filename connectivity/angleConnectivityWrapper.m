ec923377894748d990ca512428e997e82a35e37d
dharmatarha <adam.boncz@gmail.com>
2020-06-18 00:24:21 +0200

    small change

--- connectivity/angleConnectivityWrapper.m
+++ connectivity/angleConnectivityWrapper.m
@@ -39,8 +39,8 @@ function angleConnectivityWrapper(freq, varargin)
 %       subjects={'s02','s03','s04','s05','s06','s07','s08','s09'...
 %           ,'s11','s12','s13','s14','s15','s16','s17','s18','s19','s20',...
 %           's21','s22','s23','s24','s25','s26','s27','s28'}
-% method    - Connectivity measure compatible with phase data, one of 
-%           {'plv', 'iplv', 'pli'}. Defaults to 'iplv'.
+% method    - String, one of {'plv', 'iplv', 'pli'}. Specifies a 
+%       connectivity measure compatible with phase data. Defaults to 'iplv'
 % 
 %  
 
