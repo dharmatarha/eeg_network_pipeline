06308d2f654858db39e52a6ab5ef2971912a1cf6
dharmatarha <adam.boncz@gmail.com>
2020-08-10 10:44:01 +0200

    cleaned up + docs

--- plotting/modularityRealVersusNull.m
+++ plotting/modularityRealVersusNull.m
@@ -1,35 +1,67 @@
-function modularityRealVersusNull(realResFile, nullResFile, xlim, ylim)
-%% Generating heatmaps from corresponding outputs of multiCommDetectWrapper 
-% and multiCommDetectWrapper runs.
+function modularityRealVersusNull(realResFile, nullResFile, omegaLim, gammaLim)
+%% Generating heatmaps from multiCommDetect(Null)Wrapper outputs
 %
-% Inputs:
+% USAGE: modularityRealVersusNull(realResFile, 
+%                                 nullResFile,
+%                                 omegaLim=[1, length(omegaValues)], 
+%                                 gammaLim=[1, length(gammmaValues)])
+%
+% Creates and saves a series of heatmaps to depict the results of
+% corresponding multiCommDetectWrapper and multiCommDetectNullWrapper runs.
+%
+% 
+% Mandatory inputs:
 % realResFile   - Char array, path to file storing multiCommDetectWrapper
 %               outputs
 % nullResFile   - Char array, path to file storing multiCommDetectNullWrapper
 %               outputs
-% xlim          - Numeric vector, with numel(xlim)=2. X axis limits for
-%               displaying results.
-% ylim          - Numeric vector, with numel(ylim)=2. Y axis limits for
-%               displaying results.
 %
-% Outputs are heatmaps saved out into pwd.
+% Optional inputs:
+% omegaLim      - Numeric vector with two elements. X axis (omega axis) 
+%               limits for the heatmaps. Defaults to full range of values 
+%               (first and last elements of the omegaValues arrays from 
+%               the multiCommDetectWrapper/multiCommDetectNullWrapper outputs).
+% gammaLim      - Numeric vector with two elements. Y axis (gamma axis) 
+%               limits for the heatmaps. Defaults to full range of values 
+%               (first and last elements of the omegaValues arrays from 
+%               the multiCommDetectWrapper/multiCommDetectNullWrapper outputs).
 %
+% Outputs are heatmaps saved out into pwd. There are three group of
+% heatmaps:
+% (1) Heatmaps for multiCommDetectWrapper outputs; saved into files
+% starting with "heatmap_modReal_*"
+% (2) Heatmaps for multiCommDetectNullWrapper outputs; saved into files
+% starting with "heatmap_modNull_*"
+% (3) Heatmaps for the difference between the two types of outputs; saved 
+% into files starting with "heatmap_modDifference_*"
+%
+% The rest of the filenames describe the stat depicted (e.g. mean quality
+% value) and the omega / gamma limits used (x /y axis limits). E.g.
+% heatmap_modDifference_qMean_1_50_1_50.png
 %
 
 
 %% Input checks
 
-if nargin ~= 4
-    error('Function modularityRealVersusNull requires input args "realResFile", "nullResFile", "xlim" and "ylim"!');
+if ~ismembertol(nargin, 2:4)
+    error(['Function modularityRealVersusNull requires input args "realResFile" ',...
+        'and "nullResFile" while args "omegaLim" and "gammaLim" are optional!']);
 end
 if ~exist(realResFile, 'file') || ~exist(nullResFile, 'file')
     error('Input args "realResFile" and "nullResFile" must be valid file paths!');
 end
-if ~isvector(xlim) || numel(xlim)~=2
-    error('Input arg "xlim" must be a numeric vector with two elements!');
-end
-if ~isvector(ylim) || numel(ylim)~=2
-    error('Input arg "ylim" must be a numeric vector with two elements!');
+if nargin == 2
+    omegaLim = false;
+    gammaLim = false;
+elseif nargin == 3
+    omegaLim = false;
+elseif nargin == 4
+    if ~isvector(omegaLim) || numel(omegaLim)~=2
+        error('Input arg "omegaLim" must be a numeric vector with two elements!');
+    end
+    if ~isvector(gammaLim) || numel(gammaLim)~=2
+        error('Input arg "gammaLim" must be a numeric vector with two elements!');
+    end
 end
 
 
@@ -47,6 +79,14 @@ if ~strcmp(realRes.randmove, nullRes.randmove) || ~strcmp(realRes.method, nullRe
 end
 if ~isequal(realRes.postprocess, nullRes.postprocess)
     error('"Postprocess" settings were not the same in the two data sets!');
+end
+
+% get omega and gamma limits if they were not supplied
+if ~omegaLim
+    omegaLim = [1, length(realRes.omegaValues)];
+end
+if ~gammaLim
+    gammaLim = [1, length(realRes.gammaValues)];
 end
 
 
@@ -86,9 +126,9 @@ for i = 1:length(fieldn)
     
     f = figure;
     f.Position = [0 0 1800, 1080];
-    heatmap(xdata(xlim(1):xlim(2)), ydata(ylim(1):ylim(2)), diffRes.(fieldn{i})(ylim(1):ylim(2), xlim(1):xlim(2)), 'Title', [fieldn{i}, ' - ', titles{i}], 'XLabel', 'omega', 'Ylabel', 'gamma');
+    heatmap(xdata(omegaLim(1):omegaLim(2)), ydata(gammaLim(1):gammaLim(2)), diffRes.(fieldn{i})(gammaLim(1):gammaLim(2), omegaLim(1):omegaLim(2)), 'Title', [fieldn{i}, ' - ', titles{i}], 'XLabel', 'omega', 'Ylabel', 'gamma');
     
-    saveF = ['heatmap_modDifference_', fieldn{i},'_', num2str(xlim(1)), '_', num2str(xlim(2)), '_', num2str(ylim(1)), '_', num2str(ylim(2)),'.png'];
+    saveF = ['heatmap_modDifference_', fieldn{i},'_', num2str(omegaLim(1)), '_', num2str(omegaLim(2)), '_', num2str(gammaLim(1)), '_', num2str(gammaLim(2)),'.png'];
     
     saveas(f, saveF);
     
@@ -103,9 +143,9 @@ for i = 1:length(fieldn)
     
     f = figure;
     f.Position = [0 0 1800, 1080];
-    heatmap(xdata(xlim(1):xlim(2)), ydata(ylim(1):ylim(2)), realRes.res.(fieldn{i})(ylim(1):ylim(2), xlim(1):xlim(2)), 'Title', [fieldn{i}, ' - ', titles{i}], 'XLabel', 'omega', 'Ylabel', 'gamma');
+    heatmap(xdata(omegaLim(1):omegaLim(2)), ydata(gammaLim(1):gammaLim(2)), realRes.res.(fieldn{i})(gammaLim(1):gammaLim(2), omegaLim(1):omegaLim(2)), 'Title', [fieldn{i}, ' - ', titles{i}], 'XLabel', 'omega', 'Ylabel', 'gamma');
     
-    saveF = ['heatmap_modReal_', fieldn{i},'_', num2str(xlim(1)), '_', num2str(xlim(2)), '_', num2str(ylim(1)), '_', num2str(ylim(2)),'.png'];
+    saveF = ['heatmap_modReal_', fieldn{i},'_', num2str(omegaLim(1)), '_', num2str(omegaLim(2)), '_', num2str(gammaLim(1)), '_', num2str(gammaLim(2)),'.png'];
     
     saveas(f, saveF);
     
@@ -120,9 +160,9 @@ for i = 1:length(fieldn)
     
     f = figure;
     f.Position = [0 0 1800, 1080];
-    heatmap(xdata(xlim(1):xlim(2)), ydata(ylim(1):ylim(2)), nullRes.res.(fieldn{i})(ylim(1):ylim(2), xlim(1):xlim(2)), 'Title', [fieldn{i}, ' - ', titles{i}], 'XLabel', 'omega', 'Ylabel', 'gamma');
+    heatmap(xdata(omegaLim(1):omegaLim(2)), ydata(gammaLim(1):gammaLim(2)), nullRes.res.(fieldn{i})(gammaLim(1):gammaLim(2), omegaLim(1):omegaLim(2)), 'Title', [fieldn{i}, ' - ', titles{i}], 'XLabel', 'omega', 'Ylabel', 'gamma');
     
-    saveF = ['heatmap_modNull_', fieldn{i},'_', num2str(xlim(1)), '_', num2str(xlim(2)), '_', num2str(ylim(1)), '_', num2str(ylim(2)),'.png'];
+    saveF = ['heatmap_modNull_', fieldn{i},'_', num2str(omegaLim(1)), '_', num2str(omegaLim(2)), '_', num2str(gammaLim(1)), '_', num2str(gammaLim(2)),'.png'];
     
     saveas(f, saveF);
     
