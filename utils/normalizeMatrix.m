21c86d902eea3d56f4c84fc947563663d1bb2776
penagydrl <60607094+penagydrl@users.noreply.github.com>
2020-08-13 19:32:36 +0200

    Mean- and median-based matrix normalization added

--- utils/normalizeMatrix.m
+++ utils/normalizeMatrix.m
@@ -1,12 +1,17 @@
-function [normalizedMatrix] = normalizeMatrix(inputMatrix)
-%% Matrix normalization
+function [normalizedMatrix] = normalizeMatrix(inputMatrix, stat, verbose)
+%% Matrix normalization, with sum, mean, or median of all values
 % 
-% USAGE: normalizedMatrix = normalizeMatrix(inputMatrix)
+% USAGE: normalizedMatrix = normalizeMatrix(inputMatrix, stat='mean', verbose = true)
 %
-% Matrix normalization is carried out such that the sum of all elements in the normalized matrix should be one.
+% By normalization we simply mean rescaling by sum / mean / median of all elements.
 %
 % Mandatory input:
 % inputMatrix       - The matrix to be normalized
+%
+% Optional input:
+% stat              - Char array, one of {'sum', 'mean', 'median'}. 
+%                     Statistic to normalize (rescale) the values with. 
+%                     Defaults to 'mean'.
 %
 % Output:
 % normalizedMatrix  - Normalized matrix
@@ -15,20 +20,50 @@ function [normalizedMatrix] = normalizeMatrix(inputMatrix)
 
 %% Input checks
 
-% Check if the input is a matrix
-if ~ ismatrix(inputMatrix)
-    error('Input shoud be a matrix');
+if ~ismembertol(nargin, 1:3)
+    error('Function "normalizeMatrix" requires input arg "inputMatrix" while input args "stat" and "verbose" are optional!');
+end
+if nargin == 1
+    stat = 'mean';
+    verbose = true;
+elseif nargin == 2
+    stat = 'mean';
+    if ~islogical(verbose) || numel(verbose)~=1
+        error('Optional input arg "verbose" must be a logical value!');
+    end
+else    
+    if ~ismember(stat, {'sum', 'mean', 'median'})
+        error('Optional input arg "stat" must be one of {''sum'', ''mean'', ''median''}!');
+    end
+    if ~islogical(verbose) || numel(verbose)~=1
+        error('Optional input arg "verbose" must be a logical value!');
+    end
+end
+if ~isnumeric(inputMatrix) || ~ismatrix(inputMatrix)
+    error('Input arg "inputMatrix" shoud be a numeric matrix');
 end
 
 
-%% Calculation
+%% Get rescaling value
 
-if any(any(isnan(inputMatrix)))
-    warning('There were NaN values in the input matrix. We use ''omitnan'' flags, so the result will be correct.');
+% linearize
+inputLin = inputMatrix(:);
+% check for NaN
+if any(isnan(inputLin)) && verbose
+    warning('There were NaN values in the input. We use ''omitnan'' flags, so the result will be correct.');
 end
-sumOfAllElements = sum(sum(inputMatrix, 'omitnan'), 'omitnan');
-normalizedMatrix = inputMatrix / sumOfAllElements;
-
+% rescaling constant
+switch stat
+    case 'sum'
+        tmp = sum(inputLin, 'omitnan');
+    case 'mean'
+        tmp = mean(inputLin, 'omitnan');
+    case 'median'
+        tmp = median(inputLin, 'omitnan');
+end
+% normalize / rescale
+normalizedMatrix = inputMatrix/tmp;
 
 
 return
+
