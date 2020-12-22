ebe31da779bcca65b0db1166a151f05aafaf0594
dharmatarha <adam.boncz@gmail.com>
2020-12-22 14:03:09 +0100

    support for pairwise orthogonalized amplitude envelope correlation, with hardcoded lowpass filtering on the envelope data

--- connectivity/connectivityWrapperReal.m
+++ connectivity/connectivityWrapperReal.m
@@ -9,7 +9,8 @@ function connectivityWrapperReal(freq, varargin)
 %
 % Connectivity is measured either by calling functions (plv, iplv or pli) 
 % defined outside this script or as simple amplitude correlation. 
-% Only these four connectivity measures ({'plv', 'iplv', 'pli', 'ampCorr'}) 
+% Only these five connectivity measures 
+% ({'plv', 'iplv', 'pli', 'ampCorr', 'orthAmpCorr'}) 
 % are supported at the moment.
 %
 % USES PARFOR! 
@@ -42,8 +43,8 @@ function connectivityWrapperReal(freq, varargin)
 %       (e.g. 's01' for files like 's01_alpha.mat'). If empty, 
 %       we use the default cell arrray defined in
 %       "restingStateSubjects.mat" (var "subjectsRS").
-% method    - String, one of {'plv', 'iplv', 'pli', 'ampCorr'}. Specifies a 
-%       connectivity measure. Defaults to 'iplv'.
+% method    - String, one of {'plv', 'iplv', 'pli', 'ampCorr', 'orthAmpCorr'}. 
+%       Specifies a connectivity measure. Defaults to 'iplv'.
 % 
 
 
@@ -65,7 +66,7 @@ if ~isempty(varargin)
             subjects = varargin{v};
         elseif ischar(varargin{v}) && ~exist('dirName', 'var') && exist(varargin{v}, 'dir')
             dirName = varargin{v};
-        elseif ischar(varargin{v}) && ~exist('method', 'var') && ismember(varargin{v}, {'pli', 'plv', 'iplv', 'ampCorr'})
+        elseif ischar(varargin{v}) && ~exist('method', 'var') && ismember(varargin{v}, {'pli', 'plv', 'iplv', 'ampCorr', 'orthAmpCorr'})
             method = varargin{v};           
         else
             error(['There are either too many input args or they are not ',...
@@ -113,6 +114,18 @@ disp([char(10), 'First data file had ', num2str(roiNo), ' channels/ROIs, ',...
     num2str(epochNo), ' epochs and ', num2str(sampleNo), ... 
     ' samples for each epoch. Assuming same channel & sample numbers for all subjects.']);
 
+% prepare a lowpass filter if envelope correlation is used
+if ismember(method, {'ampCorr', 'orthAmpCorr'})
+    lowpassFilter = designfilt('lowpassiir',... 
+            'PassbandFrequency', 10,...
+            'StopbandFrequency', 13,... 
+            'PassbandRipple', 0.1,... 
+            'StopbandAttenuation', 50,... 
+            'SampleRate', 1000,... 
+            'MatchExactly',... 
+            'passband');
+end
+
 % clock for timing the full function run
 funcClock = tic;
 
@@ -143,12 +156,18 @@ parfor subIdx = 1:subNo
     % loop through epochs
     for epochIdx = 1:subEpochNo
         
+        % hacky workaround for temporary variable warnings in parfor loops
+        subDataPhase = nan(roiNo);
+        subDataEnv = nan(roiNo);
+        
         % get phase if arg method is a phase-based measure
         if ismember(method, {'plv', 'iplv', 'pli'})
             subDataPhase = timeSeriesToPhase(squeeze(subData(:, :, epochIdx)));  % extracts instantaneous phase from analytical signal
-        % get envelope for amplitude correlation
+        % get envelope for simple amplitude correlation
         elseif strcmp(method, 'ampCorr')
             subDataEnv = envelope(squeeze(subData(:, :, epochIdx))')';  % we keep the dim order channels/ROIs X samples
+            % filter envelopes
+            subDataEnv = filter(lowpassFilter, subDataEnv')';  % we keep the dim order channels/ROIs X samples
         end
         
         % measure depends on the arg method
@@ -163,6 +182,50 @@ parfor subIdx = 1:subNo
                 rho = corr(subDataEnv')';  % we keep the dim order channels/ROIs X samples
                 rho(tril(true(roiNo))) = NaN;  % lower triangle is set to NaN, as with symmetric phase-based measures    
                 connRes(epochIdx, :, :) = rho;
+            case 'orthAmpCorr'
+                
+                % For orthogonalized amplitude correlation we use pairwise
+                % orthogonalization of input time series. That is, (1) we loop
+                % through all time series pairings; (2) Orthogonalize them;
+                % (3) Get envelope; (4) Correlate the envelopes.
+                % At the end, the upper and lower triangles of the
+                % connectivity matrix are averaged out.
+                
+                % temp var for connectivity res
+                tmp = nan(roiNo);
+                
+                % loops trough ROIs
+                for roiOne = 1:roiNo 
+                    for roiTwo = 1:roiNo
+                        if roiOne ~= roiTwo
+                            
+                            % orthogonalize dataOne with respect to dataTwo
+                            dataOne = squeeze(subData(roiOne, :, epochIdx));  
+                            dataTwo = squeeze(subData(roiTwo, :, epochIdx));
+                            projScalar = dot(dataOne, dataTwo)/norm(dataTwo);  % scalar projection of dataOne on dataTwo
+                            projVector = projScalar*(dataTwo./norm(dataTwo));  % projection vector of dataOne on dataTwo
+                            orthVector = dataOne - projVector;  % orthogonalized dataOne
+                            % get envelope of both data 
+                            dataOneEnv = envelope(orthVector);
+                            dataTwoEnv = envelope(dataTwo);
+                            % filter envelopes
+                            dataOneEnv = filter(lowpassFilter, dataOneEnv);
+                            dataTwoEnv = filter(lowpassFilter, dataTwoEnv);
+                            % get amplitude correlation
+                            tmp(roiOne, roiTwo) = corr(dataOneEnv', dataTwoEnv');
+                            
+                        end  % if
+                    end  % for roiTwo
+                end  % for roiOne
+                
+                % average the upper and lower triangles into the upper
+                % triangle
+                tmp = (triu(tmp, 1) + tril(tmp, -1)')/2;
+                % lower triangle is set to NaN
+                tmp(tril(true(roiNo))) = NaN;  
+                % collect results in the connectivity results var
+                connRes(epochIdx, :, :) = (triu(tmp, 1) + tril(tmp, -1)')/2;
+                            
         end  % switch method
 
     end  % epochIdx for loop
