8980df8d85e6efdcce1d7fba3a597499513087e3
penagydrl <60607094+penagydrl@users.noreply.github.com>
2021-01-03 00:44:15 +0100

    Modified plotting

--- utils/withinSubjectCorr.m
+++ utils/withinSubjectCorr.m
@@ -20,11 +20,12 @@ simRes_correlation_vector = reshape(withinSub_corr, [], 1);
 matrixOfCorrValues(:, 4) = simRes_correlation_vector;
 
 [h,L,MX,MED] = violin(matrixOfCorrValues, 'x', [1 2 3 4], 'mc', [], 'medc', 'k', ...
-    'facecolor', [0, 0.4470, 0.7410; 0.8500, 0.3250, 0.0980; 0.4660, 0.6740, 0.1880; 0.6350, 0.0780, 0.1840]);
+    'facecolor', [0, 0.4470, 0.7410; 0.9290, 0.6940, 0.1250; 0.4660, 0.6740, 0.1880; 0.6350, 0.0780, 0.1840], 'facealpha', 1);
 xticks([1 2 3 4]);
 xticklabels({'PLV' 'iPLV' 'ampCorr' 'orthAmpCorr'});
 xlim([0 5]);
 ylabel('Within-subject correlation');
-title('Alpha band');
-set(gca, 'FontSize', 16);
+% title('Alpha band');
+set(gca, 'FontSize', 20);
+set(gcf, 'Color', 'w');
 grid on;
