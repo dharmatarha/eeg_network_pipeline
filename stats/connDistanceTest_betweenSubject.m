514b75ac301a568c4324cb1d55e259edaaca4f05
penagydrl <60607094+penagydrl@users.noreply.github.com>
2021-02-25 16:23:14 +0100

    Network distance between different subjects
    Testing the distance of connectivity data between different subjects

---
+++ stats/connDistanceTest_betweenSubject.m
@@ -0,0 +1,190 @@
+function [distRes] = connDistanceTest_betweenSubject(connArray, varargin)
+%% Testing the distance of connectivity data between different subjects
+%
+% USAGE: distRes = connDistanceTest_betweenSubject(connArray, metric='corr') 
+%
+% If given connectivity (adjacency) matrices for a set of epochs, across
+% multiple subjects (in input arg "connArray"), the function calculates the
+% distance of average connectivity matrices between different subjects.
+%
+% Workflow, separately performed for each subject:
+% (1) Calculate average connectivity (adjacency) matrices for each subject
+% (averaging is done across all epochs of a given subject)
+% (2) Calculate the distance of two averaged conenctivity matrices
+% for all possible pairing of all subjects.
+%
+% At the moment, support distance metrics (1) correlation (input arg
+% "metric" = 'corr'), (2) Eucledian distance ('eucl'), (3) adjacency spectral 
+% distance ('adjacencySpectral'), (4) Laplacian spectral distance ('LaplacianSpectral')
+% and (5) DeltaCon ('deltaCon', see /networkSimilarity/deltaCon.m for details).
+%
+% Mandatory inputs:
+% connArray     - 4D numeric array, with dimensions: subjects X epochs X
+%               ROIs X ROIs. Contains a connectivity matrix for each epoch
+%               of each subject. Might only contain valid data in the upper
+%               triangles of the connectivity matrices, the rest might be
+%               NaN (in case of symmetric connectivity measures).
+%
+% Optional inputs:
+% metric        - Char array, one of {'corr', 'eucl', 'adjacencySpectral', 
+%               'LaplacianSpectral', 'deltaCon'}.
+%               Distance metric for comparing connectivity matrices.
+%               adjacencySpectral, LaplacianSpectral and DeltaCon rely on 
+%               the similarly named function in
+%               /networkSimilarity. Defautls to 'corr'.
+%
+% Output:
+% distRes        - 2D numeric array sized subjects X subjects. Contains
+%               connectivity matrix distance values for all possible
+%               pairings of all subjects. Only upper triangle is populated.
+%
+% Note: Correlation is basically a similarity metric. It is converted to distance
+%       using the formula: distance = sqrt(1 - similarity).
+%       Negative correlation values are transformed to non-negative values
+%       by a scale transformation.
+
+%% Input checks
+
+% cehck no. if inputs
+if ~ismember(nargin, 1:2)
+    error('Function connDistanceTest_betweenSubject requires input arg "connArray" while arg "metric" is optional!');
+end
+% check mandatory input
+if ~isnumeric(connArray) || numel(size(connArray))~=4 || size(connArray, 3)~=size(connArray ,4)
+    error('Input rag "connArray" should be a 4D numeric array where the 3rd and 4th dimensions have the same size!');
+end
+% check optional inputs
+if ~isempty(varargin)
+    for v = 1:length(varargin)
+        if ischar(varargin{v}) && ismember(varargin{v}, {'corr', 'eucl', 'adjacencySpectral', 'LaplacianSpectral', 'deltaCon'}) && ~exist('metric', 'var')
+            metric = varargin{v};
+        else
+            error('Input could not ba mapped nicely to arg "metric"!');
+        end
+    end
+end
+% assign default values if necessary
+if ~exist('metric', 'var')
+    metric = 'corr';
+end
+
+% user message
+disp([char(10), 'Called connDistanceTest_betweenSubject with input args: ',...
+    char(10), 'Input array sized ', num2str(size(connArray)), ...
+    char(10), 'Distance metric: ', metric]);
+
+
+%% Calculate similarities
+
+% clock for whole run
+funcClock = tic;
+
+% get subject, epoch and channel/ROI numbers
+[subNo, ~, roiNo, ~] = size(connArray);
+
+% preallocate results var
+distRes = nan(subNo, subNo);
+
+% average over epochs before subject-to-subject
+% comparisons
+connArray = squeeze(mean(connArray, 2));
+
+% user message
+disp([char(10), 'Calculating...']);
+
+% loop through subjects
+for subIdx = 1:subNo
+    
+    % subject-level clock
+    subClock = tic;
+    
+    % select subject's connectivity data (averaged over epochs)
+    subMatrixA = squeeze(connArray(subIdx, :, :));
+    
+    % loop through compared subjects
+    for compSubIdx = subIdx+1 : subNo
+        
+        % select compared subject's connectivity data (averaged over
+        % epochs)
+        subMatrixB = squeeze(connArray(compSubIdx, :, :));
+        
+        % for certain metrics get symmetric adjacency matrices with zeros at diagonal
+        if ismember(metric, {'eucl', 'deltaCon'})
+            subMatrixA = triu(subMatrixA, 1) + triu(subMatrixA, 1)'; 
+            subMatrixB = triu(subMatrixB, 1) + triu(subMatrixB, 1)';
+            % also standardize the scale of connections across the two
+            % matrices to a common sum (=10)
+            subMatrixA = 10*subMatrixA./sum(subMatrixA(:));
+            subMatrixB = 10*subMatrixB./sum(subMatrixB(:));
+        elseif ismember(metric, {'adjacencySpectral', 'LaplacianSpectral'})
+            subMatrixA = triu(subMatrixA, 1) + triu(subMatrixA, 1)';
+            subMatrixB = triu(subMatrixB, 1) + triu(subMatrixB, 1)';
+        end
+
+        % calculate similarity according to arg "metric"
+        switch metric
+            
+            case 'corr'
+                % linearize upper triangles of mean connectivity matrices
+                linA = subMatrixA(triu(true(roiNo), 1));
+                linB = subMatrixB(triu(true(roiNo), 1));
+                distRes(subIdx, compSubIdx) = corr(linA, linB);              
+                
+            case 'eucl'
+%                 % similarity is based on norm of difference
+%                 simRes(subIdx, compSubIdx) = 1/(1 + norm(subMatrixA-subMatrixB, 'fro'));
+                % we use a distance measure if 'eucl' is selected
+                % (Frobenius norm)
+                distRes(subIdx, compSubIdx) = norm(subMatrixA-subMatrixB, 'fro');
+                
+            case 'adjacencySpectral'
+                [~, distRes(subIdx, compSubIdx)] = adjacencySpectralDistance(subMatrixA, subMatrixB, false);
+                
+            case 'LaplacianSpectral'
+                [~, distRes(subIdx, compSubIdx)] = laplacianSpectralDistance(subMatrixA, subMatrixB, false);
+                
+            case 'deltaCon'
+%                 % first output arg of deltaCon is similarity
+%                 simRes(subIdx, compSubIdx) = deltaCon(subMatrixA, subMatrixB, false);  % "false" is for verbosity
+                % we use a distance measure (DeltaCon distance) if
+                % 'deltaCon' is selected (second output of deltaCon.m)
+                [~, distRes(subIdx, compSubIdx)] = deltaCon(subMatrixA, subMatrixB, false);  % "false" is for verbosity                
+                
+        end  % switch metric
+        
+    end  % for compSubIdx
+    
+    % user message
+    disp([char(10), 'Done with subject ', num2str(subIdx),... 
+        ', took ', num2str(round(toc(subClock), 2)), ' secs']);
+    
+end  % for subIdx
+        
+if ismember(metric, {'corr'})
+    smallestElement = min(min(distRes));
+    distRes = (distRes - smallestElement) ./ (1-smallestElement);
+end    
+
+
+%% End message, return
+
+% user message
+disp([char(10), 'Done with everything, running the function took ',... 
+    num2str(round(toc(funcClock), 2)), ' secs']);
+
+
+return
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
