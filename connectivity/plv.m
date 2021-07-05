ec6e04b9c3918dd20f884352adcaf189f6b59a1f
dharmatarha <adam.boncz@gmail.com>
2021-07-05 13:06:49 +0200

    Refactored code for PLV calculation, based on Bruna et al. (2018). Current version is 50-100x faster than before.
    Also removed the verbosity flag as it was practically never used and kind of useless anyways.
    Will do the same to IPLV, with consequences to all code calling these ones.

--- connectivity/plv.m
+++ connectivity/plv.m
@@ -1,23 +1,24 @@
-function plvRes = plv(epochData, v)
+function plvRes = plv(data)
 %% Phase-Locking value
 %
-% USAGE: plvRes = plv(epochData, v=1)
+% USAGE: plvRes = plv(data)
 %
-% Function to calculate phase-locking values (PLVs) between a set of 
-% channels / time series. IMPORTANT: Works on phase values!
+% Function to calculate phase-locking values (PLVs) across a set of 
+% channels / ROI time series. Supports PLV calculation in time series, 
+% not across trials.
 %
-% Input(s):
-% epochData     - The input is a matrix of phase values (numeric between 
-%               -1 +1 pi) where each row is a separate channel / time 
-%               series and columns correspond to samples.  
-% v             - Verbosity. If 1, it prints to command window, 0 means
-%               silence. Default is 1.
+% Returns symmetric matrix of PLV values, not only the upper triangle.
+%
+% IMPORTANT: Expects real-valued data!
+% 
+% Mandatory input(s):
+% data          - Numeric matrix, real valued. Its dimensions are channels
+%               X samples.
 %
 % Output(s):
-% plvRes        - Matrix (sized no. of channels X no. of channels) of PLV
-%               values where entry i,j is PLV between channels i and j. As
-%               the matrix is symmetric across the diagonal, there are
-%               values only in the upper triangle, the rest is NaN.
+% plvRes        - Numeric matrix, real valued. Contains PLV values. Its 
+%               dimensions are channels X channels). Entry i,j is PLV 
+%               between channels i and j.
 %
 % Relevant papers:
 % Lachaux et al., 1999. Measuring phase synchrony in brain signals. 
@@ -25,59 +26,59 @@ function plvRes = plv(epochData, v)
 % Mormann et al., 2000. Mean phase coherence as a measure for phase 
 %   synchronization and its application to the EEG of epilepsy patients. 
 %   Physica D.
+% Bruna et al., 2018. Phase locking value revisited: teaching new tricks 
+%   to an old dog. J. Neural Eng. 
+%
+% NOTES:
+% Previous implementation was based on Mormann et al. (2000), currently
+% implements the much faster method derived in Bruna et al. (2018). 
 %
 
 
 %% Input checks
 
 % number of input args
-if nargin == 1 
-    v = 1;
-elseif nargin ~= 2
-    error('Function plv requires input arg "epochData" and optional arg "v"!');
+if nargin ~= 1
+    error('Function plv requires input arg "data"!');
 end
-% check verbosity
-if ~ismembertol(v, [0 1])
-    error('Input arg "v" is either 0 or 1!');
-end
-% check input data dimensionality
-if length(size(epochData)) ~= 2
-    error('Input arg "epochData" is expected to be a 2D matrix (channels/ROIs X samples)!');
-end
-% are input values between -pi +pi ?
-if any(any(epochData > pi)) || any(any(epochData < -pi))
-    error('Elements of input arg "epochData" are >pi or <-pi. Are you sure these are phase values?');
-end
-
-% get number of channels and samples
-channelNo = size(epochData, 1);
-sampleNo = size(epochData, 2);
-
-% user message
-if v
-    disp([char(10), 'Called plv on data with ', num2str(channelNo),... 
-        ' channels, each with ', num2str(sampleNo), ' samples']);
+% check mandatory input
+if ~isnumeric(data) || ~isreal(data) || length(size(data)) ~= 2
+    error('Input arg "data" should be a 2D numeric matrix of reals (channels X samples)!');
 end
 
 
-%% Loop across channels
+%% Calculation
 
-% preallocate results variable
-plvRes = nan(channelNo, channelNo);
+% NEW, FAST METHOD BASED ON BRUNA ET AL. (2018):
 
-% go through channels/ROIs
-for channelOne = 1:channelNo-1
-    % fill a matrix with repetitions of the data from current channel/ROI
-    d1 = repmat(epochData(channelOne, :), [channelNo-channelOne, 1]);
-    % calculate plv on matrices (= multiple channel pairings)
-    plvRes(channelOne, channelOne+1:end) = abs(sum(exp(1i*(d1-epochData(channelOne+1:end, :))), 2)/sampleNo);
-    
-end
+[~, sampleNo] = size(data);
+% get analytic signal, across samples
+dataAnalytic = hilbert(data')';  % transposes keep the dims as channels X samples
+% normalize with magnitude
+normedData = dataAnalytic ./ abs(dataAnalytic);
+% plv as matrix multiplication
+plvRes = abs(normedData * normedData') / sampleNo;
 
-% user message
-if v
-    disp(['Calculated plv for ', num2str(channelNo*(channelNo-1)/2), ' channel pairings']);
-end
+
+% % OLD, SLOWER METHOD BASED ON MORMANN ET AL. (2000):
+% 
+% [channelNo, sampleNo] = size(data);
+% % get analytic signal, across samples
+% dataAnalytic = hilbert(data')';  % transposes keep the dims as channels X samples
+% dataPhase = angle(dataAnalytic);  % phase data
+% 
+% % preallocate results variable
+% plvRes = nan(channelNo, channelNo);
+% 
+% % go through channels/ROIs
+% for currentChannel = 1:channelNo-1
+%     % fill a matrix with repetitions of the data from current channel/ROI
+%     d1 = repmat(dataPhase(currentChannel, :), [channelNo-currentChannel, 1]);
+%     % calculate plv on matrices (= multiple channel pairings)
+%     plvRes(currentChannel, currentChannel+1:end) = abs(sum(exp(1i*(d1-dataPhase(currentChannel+1:end, :))), 2)/sampleNo);
+%     
+% end
+
 
 return
 
