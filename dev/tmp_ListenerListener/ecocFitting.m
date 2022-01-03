02c64db8ee65ed8470851872c860a2c8a3da967c
dharmatarha <adam.boncz@gmail.com>
2022-01-03 04:40:09 +0100

    script for manuscript results

---
+++ dev/tmp_ListenerListener/ecocFitting.m
@@ -0,0 +1,193 @@
+%% ECOC script
+% Script to calculate the "ideal" set of edges detecting a difference in
+% context, using ECOC
+
+
+%% Base params
+
+method = 'ciplv'; 
+freq = 'alpha';
+onlyPos = true;
+
+simMetric = 'corr';
+
+% type of thresholding (if any), one of {'unthr', 'thrSub', 'thrGroup'}
+thr = 'groupv2thrSub';
+
+baseDir = '/media/NAS502/adamb/hyperscan/newSurrEdgeEstimates/';
+
+
+%% Load edge contributions data, determined by "method", "freq" and "thr"
+
+if onlyPos
+    edgeContrF = fullfile(baseDir, 'edgeContr', ['edgeContr_', freq, '_', method, '_', thr, '_onlyPos.mat']);
+else
+    edgeContrF = fullfile(baseDir, 'edgeContr', ['edgeContr_', freq, '_', method, '_', thr, '.mat']);
+end
+contrData = load(edgeContrF);
+
+% sanity check - were the edge contributions calculated with the requested
+% similarity metric?
+if ~strcmp(simMetric, contrData.simMetric)
+    error(['Bad similarity metric! Edge contributions file specified simMetric: ', contrData.simMetric]);
+end
+
+
+%% Get cohen d values and p values in connectivity map format
+
+ps = contrData.permRes.pEstAll; ps(ps==0) = 1 / contrData.permNo;
+ds = contrData.permRes.cohendAll;
+
+% in connectiviy map formats, by populating the upper triangles
+pMap = nan(62); pMap(triu(true(62), 1)) = ps;
+dMap = nan(62); dMap(triu(true(62), 1)) = ds;
+
+% FDR
+q = 0.05;
+fdrMethod = 'bh';
+[h, crit] = fdr(ps, q, fdrMethod);
+
+% apply fdr crit to maps
+pMapFdr = pMap; pMapFdr(pMap > crit) = 0;
+dMapFdr = dMap; dMapFdr(pMap > crit) = 0;
+
+% apply fdr crit to d / p values in vector format as well
+dsFdr = ds;
+dsFdr(ps > crit) = 0;
+psFdr = ps;
+psFdr(ps > crit) = 0;
+
+
+%% Load ROI label names
+
+labels = load('/home/adamb/eeg_network_pipeline/utils/roiNamesInOrder.mat');
+labels = labels.roisShort;  % short names
+
+
+%% Load connectivity data, determined by "method" and "freq"
+
+fileP = fullfile(baseDir, freq, ['group_surrResultsv2_', freq, '_', method, '.mat']);
+res = load(fileP);
+
+
+%% Get mean connectivity, depending on "thr"
+
+switch thr
+    case 'unthr'
+        connData = res.meanConn;
+    case 'groupv2thrSub'
+        % get the data from the averaged, subject-level thresholded matrices, both
+        % negative and positive diffs
+        if onlyPos
+            connData = res.meanMaskedConnPos;   
+        else
+            connData = res.meanMaskedConnPos + res.meanMaskedConnNeg; 
+        end
+    case 'groupv2thrGroup'
+        if onlyPos
+            connData = res.groupMaskedConnPos;
+        else
+            connData = res.groupMaskedConnPos + res.groupMaskedConnNeg;
+        end
+end  % switch
+        
+% get averaged conn matrix
+
+meanConn = squeeze(mean(mean(connData, 1), 2));
+
+
+%% Apply FDR to mean connectivity as well
+
+meanConnFdr = meanConn; 
+meanConnFdr(pMap > crit) = 0;
+
+
+%% Fit ECOC
+
+storyLabels = [repmat({'Story1'}, [40, 1]); repmat({'Story2'}, [40, 1]); repmat({'Story3'}, [40, 1]); repmat({'Story4'}, [40, 1])];
+connData3d = cat(1, squeeze(connData(1,:,:, :)), squeeze(connData(2,:,:, :)), squeeze(connData(3,:,:, :)), squeeze(connData(4,:,:, :)));
+%connDataLin = permute(connData3d, [2 3 1]); connDataLin = linearizeTrius(connDataLin, 1); connDataLin = connDataLin';
+dsFdrSorted = sort(dsFdr, 'descend');
+maxRuns = 100;
+maxEdges = 300;
+
+% var holding classification error rate results for each run and edge
+% number
+cer1 = zeros(maxRuns, maxEdges);
+
+% ecoc params
+kfold = 5;
+codingType = 'onevsone';
+learners = 'svm';
+verbosity = 0;
+opts = statset;
+opts.UseParallel = false;
+
+parfor topN = 1:maxEdges
+    
+        % select the top N edges as predictors
+        topCritValue = dsFdrSorted(topN);
+        ecocPred = connData3d(:, dMapFdr >= topCritValue);
+
+        % temp var for collecting results in parfor
+        tmpParfor = zeros(maxRuns);
+        
+        for runN = 1:maxRuns
+        
+            % select the top N edges as predictors
+            ecocObj = fitcecoc(ecocPred, storyLabels, 'Learners', learners,... 
+                'Coding', codingType, 'KFold', kfold, 'Verbose', verbosity,...
+                'Options', opts);
+            
+            tmpParfor(runN) = kfoldLoss(ecocObj);
+        
+        end  % for runNo
+        
+        % slice into results var
+        cer1(:, topN) = tmpparfor;
+        
+end  % parfor topNo
+
+
+
+%% Different ECOC params
+
+maxRuns = 100;
+maxEdges = 300;
+
+% var holding classification error rate results for each run and edge
+% number
+cer2 = zeros(maxRuns, maxEdges);
+
+% ecoc params
+kfold = 5;
+codingType = 'ternarycomplete';
+learners = 'svm';
+verbosity = 0;
+opts = statset;
+opts.UseParallel = false;
+
+parfor topN = 1:maxEdges
+    
+        % select the top N edges as predictors
+        topCritValue = dsFdrSorted(topN);
+        ecocPred = connData3d(:, dMapFdr >= topCritValue);
+
+        % temp var for collecting results in parfor
+        tmpParfor = zeros(maxRuns);
+        
+        for runN = 1:maxRuns
+        
+            % select the top N edges as predictors
+            ecocObj = fitcecoc(ecocPred, storyLabels, 'Learners', learners,... 
+                'Coding', codingType, 'KFold', kfold, 'Verbose', verbosity,...
+                'Options', opts);
+            
+            tmpParfor(runN) = kfoldLoss(ecocObj);
+        
+        end  % for runNo
+        
+        % slice into results var
+        cer2(:, topN) = tmpparfor;
+        
+end  % parfor topNo
