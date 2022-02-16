f810d22a8481e1df2bcb8364dda254dad840c2e1
dharmatarha <adam.boncz@gmail.com>
2022-02-16 11:24:49 +0100

    Created a simple function from the script. Permmutation number and similarity metric are now valid, hardcoded variables passed onto connSimTest_subject_thresholding explicitly

--- stats/withinSubjectCorrelationNoWrap_thresholded.m
+++ stats/withinSubjectCorrelationNoWrap_thresholded.m
@@ -1,24 +1,66 @@
+function withinSubjectCorrelationWrap_thresholded(freq, connMethod)
+%% Wrapper for calling the function connSimTest_subject_thresholding
+%
+% For a specific frequency band and connectivity method. Saves out
+% the output to a file named FREQ_CONNMETHOD_simResCorr_thr.mat. 
+% 
+% Contains hardcoded path variables.
+%
 
-frequencyBands = {'theta', 'alpha', 'beta', 'gamma', 'delta'};
-connMetrics = {'plv', 'iplv', 'ampCorr', 'orthAmpCorr'};
+
+%% Input checks
+
+if nargin ~= 2
+    error('Requires input args "freq" and "connMethod"!');
+end
+if ~ischar(freq) || ~ismember(freq, {'theta', 'alpha', 'beta', 'gamma', 'delta'})
+    error('Input arg "freq" should be a char array, one of {"delta", "theta", "alpha", "beta", "gamma"}!');
+end
+if ~ischar(freq) || ~ismember(connMethod, {'plv', 'iplv', 'ampCorr', 'orthAmpCorr'})
+    error('Input arg "connMethod" should be a char array, one of {"plv", "iplv", "ampCorr", "orthAmpCorr"}!');
+end
+
+
+%% Basic params, hardcoded values
+
 dirNameBase = '/home/peternagy/NAS502/EEG_resting_state/';
-subjectNo = 200;
-permutationNo = 1000;
+dirName = [dirNameBase freq];
 
-freqBandString = frequencyBands{1};
-connMetricString = connMetrics{2};
-dirName = [dirNameBase freqBandString];
-connectivityFileName = [freqBandString, '_', connMetricString, '_group', '.mat'];
-if ismember(connMetricString, {'plv', 'iplv'})
+% group connectivity data file
+connectivityFileName = [freq, '_', connMethod, '_group', '.mat'];
+% results file
+simResCorrFileName = [freq, '_', connMethod, '_simResCorr_thr', '.mat'];
+
+permutationNo = 1000;
+simMetric = 'corr';
+
+if ismember(connMethod, {'plv', 'iplv'})
 	truncated = 'truncated';
 else
 	truncated = 'nontruncated';
 end
-simResCorr = connSimTest_subject_thresholding([dirName, '/', connectivityFileName], ...
-dirName, freqBandString, connMetricString, truncated);
-        
-simResCorrFileName = [freqBandString, '_', connMetricString, '_simResCorr_thr', '.mat'];
+
+
+%% Call the within-subject similarity calculator function
+
+simResCorr = connSimTest_subject_thresholding([dirName, '/', connectivityFileName], ...   % connectivity data file
+                                            dirName,...                                   % surrogate data folder  
+                                            freq,...                                      % frequency band
+                                            connMethod,...                                % connectivity method
+                                            truncated,...                                 % truncated or non-truncated normals for stats
+                                            permutationNo,...                             % number of permutations for testing within-subject connectivity similarity
+                                            simMetric);                                   % similarity metric
+
+
+%% Save out resutls
+
 save([dirName, '/', simResCorrFileName], 'simResCorr');
+
+
+return
+
+
+
 
 % simResCorr_vector = reshape(simResCorr, [], 1);
 % tensorOfCorrValues(freqBandIndex, connMetricIndex, :) = simResCorr_vector;
